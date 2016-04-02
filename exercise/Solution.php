04def6132276e8884ce8ce6ba218ddd43a038ca7
Damir Rysaev <freepad@ya.ru>
2016-04-02 12:57:54 +0500

    done challenge

--- exercise/Solution.php
+++ exercise/Solution.php
@@ -1,5 +1,9 @@
 <?php
 
 // BEGIN (write your solution here)
-
+function isPalindrome($word)
+{
+    $word2 = strtolower($word);
+    return $word2 == strrev($word2);
+}
 // END
--- exercise/TestSolution.php
+++ exercise/TestSolution.php
@@ -8,5 +8,24 @@ use App\Solution\isPalindrome;
 
 class TestSolution extends \PHPUnit_Framework_TestCase
 {
+    /**
+    * @dataProvider additionProvider
+    */
+    public function testPalindrome($expected, $argument)
+    {
+        $this->assertEquals($expected, isPalindrome($argument));
+    }
 
+    public function additionProvider()
+    {
+        return [
+            [true, ''],
+            [true, 'bob'],
+            [false, 'Hexlet'],
+            [true, 101],
+            [false, 1234],
+            [false, 'world'],
+            [true, 'Anna'],
+        ];
+    }
 }
